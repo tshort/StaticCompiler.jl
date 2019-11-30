d0a583b16386c77498a0dc4ed9e70610fa2526e4
aminya <aminyahyaabadi74@gmail.com>
2019-11-30 17:15:25 -0600

    adding julia bindir to path on windows

--- test/jlrun.jl
+++ test/jlrun.jl
@@ -52,6 +52,8 @@ macro jlrun(e)
 	elseif Sys.iswindows()
 		libname = "libjulia"
 		shellcmd = ["cmd", "/c", "gcc"]
+		# libjulia needs to be in the same directory as the embedding executable or in path
+		ENV["PATH"] = string(Sys.BINDIR, ";", ENV["PATH"])
 	else
 		error("run command not defined")
 	end
