e4280407740997213564c61b45a6079438b79168
aminya <aminyahyaabadi74@gmail.com>
2019-12-01 17:51:43 -0600

    Full Cmap

--- test/standalone-exe.jl
+++ test/standalone-exe.jl
@@ -48,10 +48,29 @@ int main()
 }
 """
 
+# "signed" is removed from signed types
+# duplicates will remove automatically
 Cmap = Dict(
-    Cint => "int",
-    Clong => "long",
-    Cdouble => "double",
+    Cchar => "char",                #Int8
+    Cuchar => "unsigned char",      #UInt8
+    Cshort => "short",              #Int16
+    # Cstring =>
+    Cushort => "unsigned short",    #UInt16
+    Cint => "int",                  #Int32
+    Cuint => "unsigned int",        #UInt32
+    Clong => "long",                #Int32
+    Culong => "unsigned long",      #UInt32
+    Clonglong => "long long",       #Int64
+    Culonglong => "unsigned long long", #UInt64
+    Cintmax_t => "intmax_t",        #Int64
+    Cuintmax_t => "uintmax_t",      #UInt64
+    Csize_t => "size_t",            #UInt
+    Cssize_t => "ssize_t",          #Int
+    Cptrdiff_t => "ptrdiff_t",      #Int
+    Cwchar_t => "wchar_t",          #Int32
+    # Cwstring =>
+    Cfloat => "float",              #Float32
+    Cdouble => "double",            #Float64
     Nothing => "void",
 )
 Cformatmap = Dict(
