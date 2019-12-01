93651097dd1130435c3e39fe38bb3c562ca3ec9f
aminya <aminyahyaabadi74@gmail.com>
2019-12-01 00:12:31 -0600

    Fixes tshort#5

--- src/irgen.jl
+++ src/irgen.jl
@@ -11,7 +11,13 @@ function xlinfo(f, tt)
     sig_tt = Tuple{typeof(g), tt.parameters...}
     (ti, env) = ccall(:jl_type_intersection_with_env, Any,
                       (Any, Any), sig_tt, meth.sig)::Core.SimpleVector
-    meth = Base.func_for_method_checked(meth, ti)
+
+    if VERSION >= v"1.2.0-DEV.320"
+        meth = Base.func_for_method_checked(meth, ti, env)
+    else
+        meth = Base.func_for_method_checked(meth, ti)
+    end
+    
     return ccall(:jl_specializations_get_linfo, Ref{Core.MethodInstance},
                  (Any, Any, Any, UInt), meth, ti, env, world)
 end
