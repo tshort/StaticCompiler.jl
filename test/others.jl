4201f55c4b1762ebc7c1f5d1355555310ceee618
aminya <aminyahyaabadi74@gmail.com>
2020-01-04 21:55:42 +0330

    jlrun in others.jl

--- test/others.jl
+++ test/others.jl
@@ -4,7 +4,7 @@ mutable struct AAA
 end
 @noinline ssum(x) = x.aaa + x.bbb
 fstruct(x) = ssum(AAA(x, 99))
-@test fstruct(10) == @jlrun fstruct(10)
+@test (@jlrun fstruct(10)) == fstruct(10)
 
 module ZZ
 mutable struct AAA
@@ -15,39 +15,39 @@ end
 fstruct(x) = ssum(AAA(x, 99))
 end # module
 ffstruct(x) = ZZ.fstruct(x)
-@test ffstruct(10) == @jlrun ffstruct(10)
+@test (@jlrun ffstruct(10)) == ffstruct(10)
 
 const ag = Ref(0x80808080)
 jglobal() = ag
-@show bg = @jlrun jglobal()
+@show bg = (@jlrun jglobal())
 # @test jglobal()[] == bg[]      # Something's broken with mutable's
 
 arraysum(x) = sum([x, 1])
 # @test arraysum(6) == @jlrun arraysum(6)
 
 fsin(x) = sin(x)
-@test fsin(0.5) == @jlrun fsin(0.5)
+@test (@jlrun fsin(0.5)) == fsin(0.5)
 
 fccall() = ccall(:jl_ver_major, Cint, ())
-@test fccall() == @jlrun fccall()
+@test (@jlrun fccall()) == fccall()
 
 fcglobal() = cglobal(:jl_n_threads, Cint)
-@test fcglobal() == @jlrun fcglobal()
+@test (@jlrun fcglobal()) == fcglobal()
 
 const sv = Core.svec(1,2,3,4)
 fsv() = sv
-@test fsv() == @jlrun fsv()
+@test (@jlrun fsv()) == fsv()
 
 const arr = [9,9,9,9]
 farray() = arr
-@show @jlrun farray()
+@show (@jlrun farray())
 @show farray()
 # @test farray() == @jlrun farray()
 
 @noinline f0(x) = 3x
 @noinline fop(f, x) = 2f(x)
 funcall(x) = fop(f0, x)
-@test funcall(2) == @jlrun funcall(2)
+@test (@jlrun funcall(2)) == funcall(2)
 
 hi() = print(Core.stdout, 'X')
 @jlrun hi()
@@ -60,14 +60,14 @@ function gx(i)
     @inbounds i > 3 ? a[1] : a[5]
 end
 
-@test gx(4) == @jlrun gx(4)
+@test (@jlrun gx(4)) == gx(4)
 
 fsimple() = [0:.001:2;][end]
 
-@test fsimple() == @jlrun fsimple()
+@test (@jlrun fsimple()) == fsimple()
 
 @noinline function fsym(x; l = :hello, s = :x)
     s == :asdf ? x : 2x
 end
 gsym(x) = fsym(x, l = :hello, s = :asdf) + 1
-@test gsym(3) == @jlrun gsym(3)
+@test (@jlrun gsym(3)) == gsym(3)
