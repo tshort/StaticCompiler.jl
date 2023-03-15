080181f40fa3c99d3efab3309d3e24f5988cb21b
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-03-14 21:21:39 -0400

    Error handling tests (#98)
    * Add simple core and integration test for new error handling code
    
    * Bump version to 0.4.7

---
+++ test/scripts/throw_errors.jl
@@ -0,0 +1,15 @@
+using StaticCompiler
+using StaticTools
+
+function maybe_throw(argc::Int, argv::Ptr{Ptr{UInt8}})
+    printf(c"Argument count is %d:\n", argc)
+    argc > 1 || return printf(stderrp(), c"Too few command-line arguments\n")
+    n = argparse(Int64, argv, 2)            # First command-line argument
+    printf((c"Input:\n", n, c"\n"))
+    printf(c"\nAttempting to represent input as UInt64:\n")
+    x = UInt64(n)
+    printf(x)
+end
+
+# Attempt to compile
+path = compile_executable(maybe_throw, (Int64, Ptr{Ptr{UInt8}}), "./")
